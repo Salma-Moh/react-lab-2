b0c33627a933f29d8cd4b19582a4d44795097db8
Salma-Moh <salmamoh168@gmail.com>
2024-08-17 22:31:37 +0300

    hmm

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
 
       <div className="App">
         <Counter />
-        <CounterWithEffect />
         <FAQ />
+        <CounterWithEffect />
       </div>
     </Router>
   );
